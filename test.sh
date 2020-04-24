21a55e425339d73060e8a0eac903268a1102fb7f
Brad Huang <bradhuang1999@yahoo.com>
2020-04-24 13:06:45 -0400

    Add test inputs

---
+++ test.sh
@@ -0,0 +1,21 @@
+make
+
+echo "snc_eue: \t4x Load/3x PV: \t`./bin/snc_eue 2000 500 0.05 0.95 100 example_inputs/load_4x.txt example_inputs/pv_3x.txt`"
+echo "snc_eue: \t1x Load/1x PV: \t`./bin/snc_eue 2000 500 0.05 0.95 100 example_inputs/load_1x.txt example_inputs/pv_tmy3.txt`"
+echo "snc_eue: \t4x Load/1x PV: \t`./bin/snc_eue 2000 500 0.05 0.95 100 example_inputs/load_4x.txt example_inputs/pv_tmy3.txt`"
+echo "snc_eue: \t1x Load/3x PV: \t`./bin/snc_eue 2000 500 0.05 0.95 100 example_inputs/load_1x.txt example_inputs/pv_3x.txt`"
+
+echo "sim_eue: \t4x Load/3x PV: \t`./bin/sim 2000 500 1 0.05 0.95 100 example_inputs/load_4x.txt example_inputs/pv_3x.txt`"
+echo "sim_eue: \t1x Load/1x PV: \t`./bin/sim 2000 500 1 0.05 0.95 100 example_inputs/load_1x.txt example_inputs/pv_tmy3.txt`"
+echo "sim_eue: \t4x Load/1x PV: \t`./bin/sim 2000 500 1 0.05 0.95 100 example_inputs/load_4x.txt example_inputs/pv_tmy3.txt`"
+echo "sim_eue: \t1x Load/3x PV: \t`./bin/sim 2000 500 1 0.05 0.95 100 example_inputs/load_1x.txt example_inputs/pv_3x.txt`"
+
+echo "snc_lolp: \t4x Load/3x PV: \t`./bin/snc_lolp 2000 500 0.05 0.95 100 example_inputs/load_4x.txt example_inputs/pv_3x.txt`"
+echo "snc_lolp: \t1x Load/1x PV: \t`./bin/snc_lolp 2000 500 0.05 0.95 100 example_inputs/load_1x.txt example_inputs/pv_tmy3.txt`"
+echo "snc_lolp: \t4x Load/1x PV: \t`./bin/snc_lolp 2000 500 0.05 0.95 100 example_inputs/load_4x.txt example_inputs/pv_tmy3.txt`"
+echo "snc_lolp: \t1x Load/3x PV: \t`./bin/snc_lolp 2000 500 0.05 0.95 100 example_inputs/load_1x.txt example_inputs/pv_3x.txt`"
+
+echo "sim_lolp: \t4x Load/3x PV: \t`./bin/sim 2000 500 0 0.05 0.95 100 example_inputs/load_4x.txt example_inputs/pv_3x.txt`"
+echo "sim_lolp: \t1x Load/1x PV: \t`./bin/sim 2000 500 0 0.05 0.95 100 example_inputs/load_1x.txt example_inputs/pv_tmy3.txt`"
+echo "sim_lolp: \t4x Load/1x PV: \t`./bin/sim 2000 500 0 0.05 0.95 100 example_inputs/load_4x.txt example_inputs/pv_tmy3.txt`"
+echo "sim_lolp: \t1x Load/3x PV: \t`./bin/sim 2000 500 0 0.05 0.95 100 example_inputs/load_1x.txt example_inputs/pv_3x.txt`"
